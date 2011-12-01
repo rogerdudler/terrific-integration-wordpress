3bbe4187a44e638a22d3f7a6cbd3eb0ac79fcccc
Roger Dudler <roger.dudler@gmail.com>
2011-12-01 22:45:05 +0100

    Removed separators.

--- footer.php
+++ footer.php
@@ -10,14 +10,12 @@
 
 ?>
 </div>
-<div class="separator"><div></div></div>
 <div class="fat-footer">
  <div class="unit size1of4"><?php terrific_module('Categories', array('skin' => 'Footer', 'title' => 'Categories')); ?></div>
  <div class="unit size1of4"><?php terrific_module('Archive', array('skin' => 'Footer', 'title' => 'Archives')); ?></div>
  <div class="unit size1of4"><?php terrific_module('Navigation', array('skin' => 'Footer', 'template' => 'more')); ?></div>
  <div class="unit size1of4 lastUnit"><?php terrific_module('Navigation', array('skin' => 'Footer', 'template' => 'subscribe')); ?></div>
 </div>
-<div class="separator"><div></div></div>
 <?php terrific_module('Footer') ?>
 </div>
 <?php get_footer('empty') ?>
